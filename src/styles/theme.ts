f50c86040c7731f8c55510d9e104488322abc97d
devfaf <104205080+devfaf@users.noreply.github.com>
2026-08-17 20:41:56 +0330

    style: add design tokens

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,8 +1,27 @@
 import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
+  palette: {
+    mode: "light",
+
+    primary: {
+      main: "#0F766E",
+    },
+    secondary: {
+      main: "#F59E0B",
+    },
+
+    background: {
+      default: "#F7F8F7",
+      paper: "#FFFFFF",
+    },
+
+    text: {
+      primary: "#1F2937",
+    },
+  },
   typography: {
-    fontFamily: "var(--font-peyda)",
+    fontFamily: "var(--font-peyda), sans-serif",
   },
 });
 
