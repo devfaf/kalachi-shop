b3c3ccda3fc23e6b54aee6021c549910abd38883
devfaf <104205080+devfaf@users.noreply.github.com>
2026-09-20 23:57:46 +0330

    chore: change navbar mui codes to shadcn

--- src/components/layout/Header/Navigation/Navbar.tsx
+++ src/components/layout/Header/Navigation/Navbar.tsx
@@ -1,100 +1,79 @@
-import { Box, Button, Popper, Link } from "@mui/material";
+"use client"
+
+import {
+    NavigationMenu,
+    NavigationMenuContent,
+    NavigationMenuItem,
+    NavigationMenuLink,
+    NavigationMenuList,
+    NavigationMenuTrigger,
+} from "@/components/ui/navigation-menu";
+
 import { navItems } from "@/components/data/navItems"
 import { useState } from "react";
 import { categories } from "@/components/data/categories";
 
+
+
 export default function Navbar() {
-    const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null)
     const [activeCategoryId, setActiveCategoryId] = useState<string | null>(null)
 
     const categoryParents = categories.filter(category => !category.parentId)
 
     const categoryChildren = categories.filter(category => category.parentId === activeCategoryId)
-    
+
 
     return (
-        <Box
-            component={"ul"}
-            sx={{
-                display: "flex",
-            }}
+        <NavigationMenu
         >
-            {
-                navItems.map((item, index) => {
-                    return (
-                        <Box
-                            component="li"
-                            key={item.label}
-                            onMouseEnter={(event) => {
-                                if(item.label === "دسته‌بندی کالاها"){
-                                    setAnchorEl(event.currentTarget)
-                                }
-                            }}
+            <NavigationMenuList>
+                {
+                    navItems.map((item) => {
+                        const isCategoryMenu = item.label === "دسته‌بندی کالاها"
 
-                        >
-                            <Link href={item.href}
-                                sx={{
-                                    color: "text.primary",
-                                    display: "flex",
-                                    gap: "5px",
-                                    justifyContent: "center",
-                                    alignItems: "center",
-                                    p: "10px",
-                                    textDecoration: "none",
-                                    "&:hover": {
-                                        color: "text.primary",
-                                    }
-                                }}
-                            >
-                                <Box
-                                    component={item.icon}
-                                    sx={{
-                                        color: "text.secondary",
-                                        fontSize: "20px",
-                                    }}></Box>
-                                {item.label}
-                            </Link>
-                        </Box>
-                    )
-                })
-            }
-            <Popper
-                open={anchorEl !== null}
-                anchorEl={anchorEl}
-                placement="bottom-end"
-                onMouseLeave={() => {
-                    setAnchorEl(null);
-                }}
-            >
-                <Box
-                    sx={{
-                        display: "flex",
-                        flexDirection: "column",
-                        border: "1px solid",
-                        borderColor: "divider",
-                        bgcolor: "background.paper",
-                        p: 2,
-                        borderRadius: "0 0 16px 16px",
-                    }}
-                >
-                    {
-                        categoryParents.map((child) => (
-                            <Button
-                                key={child.id}
-                                onMouseEnter={() => setActiveCategoryId(child.id)}
-                            >
-                                {child.name}
-                            </Button>
-                        ))
-                    }
-                </Box>
-                {categoryChildren.map(submenu => (
-                    <Box key={submenu.id}>
-                        {submenu.name}
-                    </Box>
-                ))
+                        if (isCategoryMenu) {
+                            return (
+                                <NavigationMenuItem key={item.label}>
+                                    <NavigationMenuTrigger>
+                                        {item.label}
+                                    </NavigationMenuTrigger>
+                                    <NavigationMenuContent>
+                                        {
+                                            categoryParents.map((child) => (
+                                                <NavigationMenuItem
+                                                    key={child.id}
+                                                    onMouseEnter={() => setActiveCategoryId(child.id)}
+                                                >
+                                                    {child.name}
+                                                </NavigationMenuItem>
+                                            ))
+                                        }
+                                        {
+                                            categoryChildren.map(submenu => (
+                                                <NavigationMenuItem
+                                                    key={submenu.id}>
+                                                    <NavigationMenuLink>
+                                                        {submenu.name}
+                                                    </NavigationMenuLink>
+                                                </NavigationMenuItem>
+                                            ))
+                                        }
+                                    </NavigationMenuContent>
+                                </NavigationMenuItem>
+
+                            )
+                        }
+                        return (
+                            <NavigationMenuItem key={item.label}>
+                                <NavigationMenuLink href={item.href}>
+                                    {item.label}
+                                </NavigationMenuLink>
+                            </NavigationMenuItem>
+                        );
+                    })
                 }
-            </Popper>
-        </Box >
+
+            </NavigationMenuList>
+        </NavigationMenu >
     );
 }
