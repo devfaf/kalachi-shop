8fa206275a7ffeb3e26321e31194d09ed10ef5cd
devfaf <104205080+devfaf@users.noreply.github.com>
2026-09-22 23:45:47 +0330

    chore: change syntax

--- src/components/category/CategoryProductGrid.tsx
+++ src/components/category/CategoryProductGrid.tsx
@@ -1,6 +1,22 @@
-const CategoryProductGrid = () => {
-  return (
-    <div>CategoryProductGrid</div>
-  )
+import ProductCard from "@/components/product/ProductCard";
+import type { Product } from "@/data/products";
+
+type CategoryProductGridProps = {
+    products: Product[];
+};
+
+const CategoryProductGrid = ({
+    products,
+}: CategoryProductGridProps) => {
+    return (
+        <div className="grid grid-cols-2 gap-4 md:grid-cols-3 lg:grid-cols-4">
+            {products.map((product) => (
+                <ProductCard
+                    key={product.id}
+                    product={product}
+                />
+            ))}
+        </div>
+    );
 }
 export default CategoryProductGrid
